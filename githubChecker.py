f95c4681531366536f26558e2114ec4d739c9458
yifatmakias <yifatmakias@gmail.com>
2019-04-08 15:06:02 -0700

    added docker mysql, creating a database, table and entering rows.

--- githubChecker.py
+++ githubChecker.py
@@ -2,10 +2,29 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
 import unittest
 import requests as req
+import start_docker
+import pymysql
+import socket
+
+# start mysql docker database
+start_docker.main()
+ip = socket.gethostbyname(socket.gethostname())
+conn = pymysql.connect(
+    host=ip,
+    port=7200,
+    user='root',
+    password='',
+    charset='utf8mb4',
+    cursorclass=pymysql.cursors.DictCursor
+)
+conn.cursor().execute("create database if not exists testDB;")
+conn.cursor().execute("use testDB;")
+conn.cursor().execute("create table if not exists search_results(ID INT not null primary key, title text, description text, tags text, time text, language text, stars text);")
 
 
 class GithubChecker(unittest.TestCase):
     def setUp(self):
+
         # creates a new chrome session
         self.driver = webdriver.Chrome(ChromeDriverManager().install())
 
@@ -15,6 +34,7 @@ class GithubChecker(unittest.TestCase):
         # Go to github.com
         driver.get("https://www.github.com")
         driver.maximize_window()
+        driver.implicitly_wait(4)
 
         # Search selenium
         search_element = driver.find_element_by_name("q")
@@ -27,7 +47,7 @@ class GithubChecker(unittest.TestCase):
         backend_performance = response_start - navigation_start
         frontend_performance = dom_complete - response_start
 
-        # Analise results
+        # Analise results and insert them to the db
         result_list = self.driver.find_element_by_class_name("repo-list")
         items = result_list.find_elements_by_tag_name("li")
         counter = 1
@@ -42,6 +62,10 @@ class GithubChecker(unittest.TestCase):
             language_xpath = '//*[@id="js-pjax-container"]/div/div[3]/div/ul/li['+str(counter)+']/div[2]/div[1]'
             language = item.find_element_by_xpath(language_xpath).text
             stars = item.find_element_by_class_name("muted-link").text
+            tags_text =''
+            for tag in tags:
+                tags_text = tags_text + ", " + tag.text
+            conn.cursor().execute("insert ignore into search_results (ID, title, description, tags, time, language, stars) values (%s, %s, %s, %s, %s, %s, %s);", (str(counter), title, description, tags_text, time_text, language, stars))
             print("Title: " + title)
             print("Description: " + description)
             print("Tags: ")
@@ -60,7 +84,7 @@ class GithubChecker(unittest.TestCase):
             href = link.get_attribute("href")
             resp = req.get(href)
             assert int(resp.status_code) < 400
-        print("All links are valid")
+        print("All links are valid\n")
 
         # Time performance
         print("Performance time:")
@@ -68,4 +92,5 @@ class GithubChecker(unittest.TestCase):
         print("Front End: %s ms" % frontend_performance)
 
     def tearDown(self):
+        conn.commit()
         self.driver.quit()
